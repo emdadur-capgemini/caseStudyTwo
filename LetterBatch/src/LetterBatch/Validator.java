92ff5cf372f45232d121ac218d7c4a7df7f7ee5b
NAME <EMAIL>
2019-09-10 12:00:55 +0100

    12:00 10 sept: firdows

--- LetterBatch/src/LetterBatch/Validator.java
+++ LetterBatch/src/LetterBatch/Validator.java
@@ -31,23 +31,48 @@ public class Validator {
 	private static boolean ValCompDetails (String companyName, String contactName, String contactNumber){
 		if (CompanyName(companyName) == true){
 			if (contactName.length()>1 || contactName.length()<20) {
-				if (contactNumber.matches("\\d{4}+\\s+\\d{3}+\\s+\\d{4}")||(contactNumber.length()== 11)){
-					if (contactNumber.matches("\\d{4}+\\s+\\d{3}+\\s+\\d{4}[0-9]")||(contactNumber.matches("\\d{11}[0-9]"))){
-						return true;
-					}
-						// && (contactName.length()>0 || contactName.length()<14)) 
+				if (contactNumber.length()<14) {
 					return true;
+
 				}else return false;
 			}else return false;
 		}else return false;
 
 	}
-	//
-	//private static boolean ValDiscountRate (String companyName, String letterContactNme, int discountRate){}
-	//
-	//private static boolean ValInvoiceHeader (String companyName, String letterContactNme){}
-	//
-	//private static boolean ValInvoice (String companyName, String productName, double price){}
+	
+	private static boolean ValDiscountRate (String companyName, String letterContactNme, double discountRate){
+		if (CompanyName(companyName) == true){
+			if (LetterContactName(letterContactNme) == true) {
+				if (discountRate >1 && discountRate<50) {
+					return true;
+				}else return false;
+			}else return false;
+
+		}else return false;
+
+	}
+
+	private static boolean ValInvoiceHeader (String companyName, String letterContactNme){
+		if (CompanyName(companyName) == true){
+			if (LetterContactName(letterContactNme) == true) {
+				return true;
+			}else return false;
+		}else return false;
+	}
+	
+	private static boolean ValInvoice (String companyName, String productName, double price){
+		if (CompanyName(companyName) == true){
+			if (productName.length()>3 && productName.length()<50) {
+				if (price >0) {
+					if (price*100 == (int)(price*100)) {
+						return true;
+					}else return false;
+					
+					
+				}else return false;
+			}else return false;
+		}else return false; 
+	}
 
 	private static boolean CompanyName (String companyName){
 		if (companyName.length()<3 || companyName.length()>20) {
@@ -66,16 +91,27 @@ public class Validator {
 
 	}
 
-
-
+	private static boolean checknumber(String number) {
+		if (number.matches("[0-9 ]") && number.length()<14) {
+			return true;
+		}else return false;
+	}
+//"[0-9]+" 
 
 
 	public static void main(String []args) {
 
 		//System.out.println(ValRegistration("firdows", "1 telford street","ws10 8ur", "firdows"));
-		System.out.println(ValCompDetails("firdows", "1 telford street","ooooooooooo"));
+		//System.out.println(ValCompDetails("firdows", "1 telford street", "0121 526 6878"));
+		//System.out.println(ValDiscountRate("s", "fi", 6));
+		//System.out.println(ValInvoiceHeader("",""));
+		//System.out.println(ValInvoice("firdows","seldge hammer", 23.12));
+		System.out.println(checknumber("0121 832 7392"));
+
 
 	}
 
 
 }
+
+//if (contactNumber.matches("\\d{4}+\\s+\\d{3}+\\s+\\d{4}")&&(contactNumber.length()< 14)){
