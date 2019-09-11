733a554abec1e11754283a2aedbbea5dce5761fe
NAME <EMAIL>
2019-09-11 14:34:16 +0100

    14:32 (11/09/2019) Firdows - company number validation fixed and company type validation added.

--- LetterBatch/src/LetterBatch/Validator.java
+++ LetterBatch/src/LetterBatch/Validator.java
@@ -3,8 +3,13 @@ package LetterBatch;
 public class Validator {
 
 
-	private static String ValRegistration (String companyName, String address, String postCode, String letterContactNme){
+	private static String ValRegistration (String companyType, String companyName, String address, String postCode, String letterContactNme){
 		String error = "Error with ";
+		String cType = "1";
+
+		if (!companyType.equals(cType))
+			error = error+"Company type";
+
 		if (CompanyName(companyName) == false)
 			error = error+"company name";
 
@@ -21,29 +26,35 @@ public class Validator {
 			return "Correct";}
 		else return error;
 
-
 	}
 
 
-	private static String ValCompDetails (String companyName, String contactName, String contactNumber){
-
+	private static String ValCompDetails (String companyType, String companyName, String contactName, String contactNumber){
 		String error = "Error with ";
+		String cType = "1A";
+
+		if (!companyType.equals(cType))
+			error = error+"Company type";
+
 		if (CompanyName(companyName) == false)
 			error = error+"company name";
 		if (contactName.length()<1 || contactName.length()>20)
 			error = error +"Contact Name";
-		if (contactNumber.matches("^[0-9 ]+") && contactNumber.length()>14)
+		if (!contactNumber.matches("(\\(?\\+44\\)?\\s?(1|2|3|7|8)\\d{3}|\\(?(01|02|03|07|08)\\d{3}\\)?)\\s?\\d{3}\\s?\\d{3}|(\\(?\\+44\\)?\\s?(1|2|3|5|7|8)\\d{2}|\\(?(01|02|03|05|07|08)\\d{2}\\)?)\\s?\\d{3}\\s?\\d{4}|(\\(?\\+44\\)?\\s?(5|9)\\d{2}|\\(?(05|09)\\d{2}\\)?)\\s?\\d{3}\\s?\\d{3}"))
 			error = error +"Contact Number";
-
 		if (error.equals("Error with ")) {
 			return "Correct";}
 		else return error;
 
 	}
 
-	private static String ValDiscountRate (String companyName, String letterContactNme, double discountRate){
+	private static String ValDiscountRate (String companyType, String companyName, String letterContactNme, double discountRate){
 
 		String error = "Error with ";
+		String cType = "2";
+
+		if (!companyType.equals(cType))
+			error = error+"Company type";
 
 		if (CompanyName(companyName) == false)
 			error = error+"company name";
@@ -61,43 +72,49 @@ public class Validator {
 
 	}
 
-	private static String ValInvoiceHeader (String companyName, String letterContactNme){
-		
+	private static String ValInvoiceHeader (String companyType, String companyName, String letterContactNme){
+
 		String error = "Error with ";
+		String cType = "3";
+
+		if (!companyType.equals(cType))
+			error = error+"Company type";
 
 		if (CompanyName(companyName) == false)
 			error = error+"company name ";
 
 		if (LetterContactName(letterContactNme) == false)
 			error = error +"Letter Contact Name";
-		
+
 		if (error.equals("Error with ")) {
 			return "Correct";}
 		else return error;
-		
+
 	}
 
-	private static String ValInvoice (String companyName, String productName, double price){
+	private static String ValInvoice (String companyType, String companyName, String productName, double price){
 
 		String error = "Error with ";
+		String cType = "3A";
+
+		if (!companyType.equals(cType))
+			error = error+"Company type";
 
 		if (CompanyName(companyName) == false)
 			error = error+"company name ";
-		
+
 		if (productName.length()<3 && productName.length()>50)
 			error = error+"product name ";
-		
+
 		//if (price <0) {
 		if ((price <0)||(price*100 != (int)(price*100)))
-				error = error+"price ";
-			
-				
-		
-		
+			error = error+"price ";	
+
+
 		if (error.equals("Error with ")) {
 			return "Correct";}
 		else return error;
-		
+
 	}
 
 	private static boolean CompanyName (String companyName){
@@ -112,25 +129,19 @@ public class Validator {
 		if (letterContactNme.length()<1 || letterContactNme.length()>15) {
 			return false;
 		}else {
+
 			return true;
 		}
 
 	}
 
-	
-
-
 	public static void main(String []args) {
 
-		System.out.println(ValRegistration("firdows", "1 telford street","ws10 8ur", "firdows"));
-		System.out.println(ValCompDetails("firdows", "1 telford street", "0121526878"));
-		System.out.println(ValDiscountRate("s", "fi", 6));
-		System.out.println(ValInvoiceHeader("",""));
-		System.out.println(ValInvoice("firdows","seldge hammer", 1));
-
-		
-
-
+		System.out.println(ValRegistration("1", "firdows", "1 telford street","tf1 6sp", "firdows"));
+		System.out.println(ValCompDetails("1A", "firdows", "1 telford street", "0121 456 9213"));
+		System.out.println(ValDiscountRate("2", "capgemini", "fi", 6));
+		System.out.println(ValInvoiceHeader("3", "FIDOWS","firdows"));
+		System.out.println(ValInvoice("3A","firdows","seldge hammer", 1));
 
 	}
 
