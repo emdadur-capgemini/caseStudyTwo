9504fbb83b1a99033bff3639c88a24144b9a1df2
emdadur-capgemini <emdadur.rahman@capgemini.com>
2019-09-11 09:13:00 +0100

    Partial pooling included - emdad

--- LetterBatch/src/LetterBatch/LetterBatch.java
+++ LetterBatch/src/LetterBatch/LetterBatch.java
@@ -1,11 +1,18 @@
 package LetterBatch;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class LetterBatch {
 
 	public static void main (String[] args) {
 		Reader read = new Reader();
 		try {
+	        Path dir = Paths.get("/home/regen/git/caseStudyTwo/LetterBatch/resources");
+	        new Pooling(dir).processEvents();
+	        
 			read.readFile();
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
