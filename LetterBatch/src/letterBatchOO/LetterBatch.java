f1b763e9f554692a7a90fb3b9a279d8796be89da
emdadur-capgemini <emdadur.rahman@capgemini.com>
2019-09-12 15:31:32 +0100

    test

--- LetterBatch/src/letterBatchOO/LetterBatch.java
+++ LetterBatch/src/letterBatchOO/LetterBatch.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public class LetterBatch {
 	public static void main (String[] args) {
-		//get directory from Polling class
+		//??get directory from Polling class
 		Path directory = Paths.get("/home/regen/git/caseStudyTwo/LetterBatch/resources/COMPANYX.txt");
         
 		Reader inputFile = new Reader(directory);
